7132979f728ac8f64338cde732a06114b627500d
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2021-01-21 14:42:52 -0500

    Post, Update, Delete, and Listener added

--- server.js
+++ server.js
@@ -28,4 +28,47 @@ app.get("/", function(req,res){
 
 //POST
 
+app.post("/api/burgers/:id", function(req, res){
+    connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [req.body.burger], function(err, data){
+        if (err) {
+            return res.status(500).end();
+        }
+        res.json({ id: result.insertId });
+        console.log({ id: result.insertId});
+    });
+});
+
+//UPDATE
+
+app.put("/api/burgers/:id", function(req, res){
+    connection.query("UPDATE burgers SET burger_name = ? WHERE id = ?", [req.body.burger, req.params.id], function (err, data){
+        if (err) {
+            return res.status(500).end();
+        }
+        else if (result.changedRows === 0) {
+            return res.status(404).end();
+        }
+        res.status(200).end();
+    });
+});
+
+//DELETE
+
+app.delete("api/burgers/:id", function (req, res) {
+    connection.query("DELETE FROM burgers WHERE id = ?", [req.params.id], function (err, data){
+        if (err) {
+            return res.status(500).end();
+        }
+        else if (result.affectedRows === 0) {
+            return res.status(404).end();
+        }
+        res.status(200).end();
+    });
+});
+
+//Listener
+
+app.listen(PORT, function (){
+    console.log("Server listening on " + PORT);
+});
 
