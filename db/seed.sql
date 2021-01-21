d1f7d2b7557f5809dc0e34f8751e31e513b0b637
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2021-01-21 14:05:47 -0500

    Schema created. Seed created

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+DELETE FROM `burgers_db`.`burgers`;
+INSERT INTO `burgers_db`.`burgers`(`id`, `burger_name`, `devoured`) VALUES ('1', 'Veggie Burger', True);
+INSERT INTO `burgers_db`.`burgers`(`burger_name`, `devoured`) VALUES ('Big Mac', False);
+INSERT INTO `burgers_db`.`burgers`(`burger_name`, `devoured`) VALUES ('Slider', True);
+INSERT INTO `burgers_db`.`burgers`(`burger_name`, `devoured`) VALUES ('Double Quarter Pounder', False);
