bdd88e232cb940f9a42b6ba1262bd949ee3998a1
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2021-01-26 17:13:54 -0500

    Adding JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,12 @@
 const mysql = require('mysql');
 
 //MySQL connection
-var connection = mysql.createConnection({
+var connection;
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} 
+else{
+connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
